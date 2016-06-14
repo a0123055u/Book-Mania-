8421d50b3663d9bc8d2370131ff06a41265bf653
suchitaj3 <suchitaj3@gmail.com>
2016-06-14 18:20:36 +0800

    book and user domain

--- BookMania/grails-app/domain/bookmania/Book.groovy
+++ BookMania/grails-app/domain/bookmania/Book.groovy
@@ -3,8 +3,17 @@ package bookmania
 class Book {
 
 	String name
+    	String author
+    	String category
+    	String dity
+    	String isbn
+    	String publisher
+    	int copy
 	 
-
+	static belongsTo = [user: User]
     static constraints = {
+    	
+
+    	//static belongsTo 
     }
 }
---
+++ BookMania/grails-app/domain/bookmania/User.groovy
@@ -0,0 +1,15 @@
+package bookmania
+
+class User {
+
+	String name
+        String email
+        int phoneNumber
+        String City
+	 
+
+    static constraints = {
+
+
+    }
+}
